9caa62deaa96253d218933d46127c19d8de303e2
Chalotte026 <133754432+Chalotte026@users.noreply.github.com>
2023-11-21 23:53:04 +0700

    Champ แก้ menubar เสร็จแล้ว

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,33 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './header.css'
 /* import { Link } from 'react-router-dom' */
 /* import { nav } from '../../data/Data' */
 
 const Header = () => {
+    useEffect(() => {
+        // ฟังก์ชันที่จะถูกเรียกเมื่อเกิดเหตุการณ์ scroll
+        const handleScroll = () => {
+            let menuLi = document.querySelectorAll('nav ul li a');
+            let section = document.querySelectorAll('section');
+            let len = section.length;
+            while (--len && window.scrollY + 97 < section[len].offsetTop) { }
+            menuLi.forEach(sec => sec.classList.remove('active'));
+            menuLi[len].classList.add("active");
+        };
+        const handleClick = () => {
+            let menuIcon = document.querySelector("#menu-icon");
+            let navlist = document.querySelector('.nav-links');
+
+            menuIcon.onclick = () => {
+                menuIcon.classList.toggle('bx-x');
+                navlist.classList.toggle("open");
+            };
+
+            window.onscroll = () => {
+                menuIcon.classList.remove('bx-x');
+                navlist.classList.remove("open");
+            };
+        };
+        // เพิ่ม event listener สำหรับ scroll event เมื่อ component ถูก render
+        window.addEventListener('scroll', handleScroll);
+        handleClick();
+        // ฟังก์ชันที่จะถูกเรียกเมื่อ component ถูก unmount เพื่อลบ event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, []); // ให้ useEffect รันเพียงครั้งเมื่อ component ถูก render ครั้งแรกเท่านั้น
+
     return <>
         <header>
-            <div className="container flex">
+
+            <div className="navbar">
                 <div className="logo">
                     <h1>Res <span>Q</span> connect</h1>
                 </div>
-                <div className="nav">
-                    <ul className="flex">
-                        <li><a href="/">หน้าหลัก</a></li>
+                <div className="button" >
+                    <ul className="nav-links ">
+                        <li ><a href="/">หน้าหลัก</a></li>
                         <li><a href="/โครงการ">โครงการ</a></li>
                         <li><a href="/เกี่ยวกับเรา">เกี่ยวกับเรา</a></li>
                         <li><a href="/ช่องทางติดต่อ">ช่องทางติดต่อ</a></li>
                     </ul>
-                </div>
-                <div className="button flex">
                     <button className="btn1">
-                        <i class='bx bx-log-in-circle'></i><p>Sign In</p> 
+                        <i class='bx bx-log-in-circle'></i><p>Sign In</p>
                     </button>
+                    <div id="menu-icon" class="bx bx-menu"></div>
                 </div>
-                {/* <div className="toggle">
-                    <button>
-                        <i class='bx bx-menu'></i>
-                    </button>
-                </div> */}
+
             </div>
         </header>
     </>
