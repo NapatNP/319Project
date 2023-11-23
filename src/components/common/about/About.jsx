83f45c93f90ed99bd7b87aa55a1c6ab82717db5c
NapatNP <napat.piyak@bumail.net>
2023-11-23 15:51:53 +0700

    อัพเดท about รอบที่2

--- src/components/common/about/About.jsx
+++ src/components/common/about/About.jsx
@@ -3,7 +3,7 @@ import './about.css'
 
 const About = () => {
     return (
-        <div className="card">
+/*         <div className="card">
             <img src="./favicon.ico" alt="Your Name" className="profile-image" />
             <h1>About Me</h1>
 
@@ -36,7 +36,30 @@ const About = () => {
                 <p>ขอบคุณที่สนับเว็บไซต์ของเรา</p>
                 <p>Thank you for visiting our site</p>
             </footer>
+        </div> */
+
+        <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./favicon.ico" alt="About Me" />
         </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <p className="section--title">เกี่ยวกับเรา</p>
+          
+            <h1 className="skills-section--heading">Res Q Connect</h1>
+
+            <p className="hero--section-description">
+              เราเป็นทีมผู้ที่มีประสบการณ์และความมุ่งมั่นในการให้บริการช่วยเหลือที่ทันสมัยและเหมาะสมสำหรับผู้ที่ต้องการความช่วยเหลือในสถานการณ์ฉุกเฉิน ทางเราได้นำเสนอแหล่งข้อมูลที่สมบูรณ์แบบเกี่ยวกับการเตรียมตัวและการรับมือกับสถานการณ์ฉุกเฉินต่าง ๆ ซึ่งสามารถให้ข้อมูลและแนวทางที่เป็นประโยชน์ทั้งในระหว่างภัยพิบัติและหลังภัยพิบัติ เพื่อให้ผู้ใช้สามารถตัดสินใจและรับมือกับสถานการณ์ได้อย่างมั่นใจ
+            </p>
+            <p className="hero--section-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+              recusandae?
+            </p>
+          </div>
+        </div>
+      </section>
+
     );
 };    
 export default About
