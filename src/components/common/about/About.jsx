70ceb7c39885b9adfbd251596fa30b03edb0acdd
NapatNP <napat.piyak@bumail.net>
2023-11-28 16:50:00 +0700

    แก้ไข team , about

--- src/components/common/about/About.jsx
+++ src/components/common/about/About.jsx
@@ -2,64 +2,67 @@ import React from "react";
 import './about.css'
 
 const About = () => {
-    return (
-/*         <div className="card">
-            <img src="./favicon.ico" alt="Your Name" className="profile-image" />
-            <h1>About Me</h1>
+  return (
+    /*         <div className="card">
+                <img src="./favicon.ico" alt="Your Name" className="profile-image" />
+                <h1>About Me</h1>
+    
+                <div className="card-section">
+                    <h2>Introduction</h2>
+                    <p>เป็นเว็บที่มุ่งมั่นที่จะให้ความช่วยเหลือและสนับสนุนผู้ที่ได้รับผลกระทบจากภัยพิบัติและภัยธรรมชาติ เป็นเว็บที่มุ่งมั่นที่จะให้ความช่วยเหลือและสนับสนุนผู้ที่ได้รับผลกระทบจากภัยพิบัติและภัยธรรมชาติเชื่อในพลังของชุมชนและการร่วมมือกันเพื่อสร้างชีวิตที่สดใสและมีความหวังมากขึ้น</p>
+                </div>
+    
+                <div className="card-section">
+                    <h2>Experience</h2>
+                    <p>เรามีส่วนร่วมในกิจกรรมช่วยเหลือภัยพิบัติมาเป็นเวลา xx ปี รวมถึงการทำงานกับองค์กร xxxxxxx และการมีส่วนร่วมในโครงการต่างๆ ที่มุ่งเน้นให้การช่วยเหลือทันทีและการฟื้นฟูระยะยาว</p>
+                </div>
+    
+                <div className="card-section">
+                    <h2>Skill</h2>
+                    <ul>
+                        <li>การตอบสนองฉุกเฉิน</li>
+                        <li>การสื่อสารกับชุมชน</li>
+                        <li>การประสานงานด้านโลจิสติกส์</li>
+                    </ul>
+                </div>
+    
+                <div className="card-section">
+                    <h2>Contact</h2>
+                    <p>Email: abc@bumail.net</p>
+                    <p>โทร: 012-345-6789</p>
+                </div>
+    
+                <footer>
+                    <p>ขอบคุณที่สนับเว็บไซต์ของเรา</p>
+                    <p>Thank you for visiting our site</p>
+                </footer>
+            </div> */
 
-            <div className="card-section">
-                <h2>Introduction</h2>
-                <p>เป็นเว็บที่มุ่งมั่นที่จะให้ความช่วยเหลือและสนับสนุนผู้ที่ได้รับผลกระทบจากภัยพิบัติและภัยธรรมชาติ เป็นเว็บที่มุ่งมั่นที่จะให้ความช่วยเหลือและสนับสนุนผู้ที่ได้รับผลกระทบจากภัยพิบัติและภัยธรรมชาติเชื่อในพลังของชุมชนและการร่วมมือกันเพื่อสร้างชีวิตที่สดใสและมีความหวังมากขึ้น</p>
-            </div>
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./favicon.ico" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">เกี่ยวกับเรา</p>
 
-            <div className="card-section">
-                <h2>Experience</h2>
-                <p>เรามีส่วนร่วมในกิจกรรมช่วยเหลือภัยพิบัติมาเป็นเวลา xx ปี รวมถึงการทำงานกับองค์กร xxxxxxx และการมีส่วนร่วมในโครงการต่างๆ ที่มุ่งเน้นให้การช่วยเหลือทันทีและการฟื้นฟูระยะยาว</p>
-            </div>
+          <h1 className="skills-section--heading">Res Q Connect</h1>
 
-            <div className="card-section">
-                <h2>Skill</h2>
-                <ul>
-                    <li>การตอบสนองฉุกเฉิน</li>
-                    <li>การสื่อสารกับชุมชน</li>
-                    <li>การประสานงานด้านโลจิสติกส์</li>
-                </ul>
-            </div>
+          <p className="hero--section-description">
+            เราเป็นตัวแทนที่มีความมุ่งมั้นและตั้งใจให้ความช่วยเหลือกับผู้ที่ประสบภัยและต้องการความช่วยเหลือ โดยเราได้เปิดรับบริจาคเพื่อสนับสนุนกิจกรรม
+            จิตอาสาต่างๆและเรายินดีต้อนรับทุกท่านที่ต้องการร่วมเป็นส่วนหนึ่งในการสนับสนุนพวกเรา Res Q Connect ในการกระทำดีๆ เพื่อช่วยเหลือสังคมในช่วงเวลาที่ทุกคนต้องการความช่วยเหลือ
+          </p>
+          <p className="hero--section-description">
+            นอกจากนี้ เรายังเปิดโอกาสให้คนที่มีใจรักการช่วยเหลือมีโอกาสร่วมมือกับทีมของเราในกิจกรรมต่างๆ เพื่อสร้างความเข้มแข็งในการตอบสนองต่อสถานการณ์ฉุกเฉินและพร้อมที่จะให้ความช่วยเหลือที่เป็นประโยชน์ที่สุดให้กับผู้ที่ต้องการ
+          </p>
+          <p className="hero--section-description">
+            ทีมของเรามุ่งเน้นในการให้ข้อมูลและแนวทางที่เป็นประโยชน์ทั้งในช่วงภัยพิบัติและหลังภัยพิบัติ เพื่อให้ทุกท่านสามารถตัดสินใจและรับมือกับสถานการณ์ได้อย่างถูกต้องและปลอดภัย พวกเรายินดีให้คำปรึกษาและสนับสนุนทุกท่านที่ต้องการความช่วยเหลือในทุกปัจจุบันของชีวิตที่ท้าทาย หวังว่าทุกคนจะร่วมมือกันสร้างสังคมที่แข็งแกร่งและเข้มแข็งขึ้นไปพร้อมๆ กัน
+          </p>
 
-            <div className="card-section">
-                <h2>Contact</h2>
-                <p>Email: abc@bumail.net</p>
-                <p>โทร: 012-345-6789</p>
-            </div>
-
-            <footer>
-                <p>ขอบคุณที่สนับเว็บไซต์ของเรา</p>
-                <p>Thank you for visiting our site</p>
-            </footer>
-        </div> */
-
-        <section id="AboutMe" className="about--section">
-        <div className="about--section--img">
-          <img src="./favicon.ico" alt="About Me" />
         </div>
-        <div className="hero--section--content--box about--section--box">
-          <div className="hero--section--content">
-            <p className="section--title">เกี่ยวกับเรา</p>
-          
-            <h1 className="skills-section--heading">Res Q Connect</h1>
+      </div>
+    </section>
 
-            <p className="hero--section-description">
-              เราเป็นทีมผู้ที่มีประสบการณ์และความมุ่งมั่นในการให้บริการช่วยเหลือที่ทันสมัยและเหมาะสมสำหรับผู้ที่ต้องการความช่วยเหลือในสถานการณ์ฉุกเฉิน ทางเราได้นำเสนอแหล่งข้อมูลที่สมบูรณ์แบบเกี่ยวกับการเตรียมตัวและการรับมือกับสถานการณ์ฉุกเฉินต่าง ๆ ซึ่งสามารถให้ข้อมูลและแนวทางที่เป็นประโยชน์ทั้งในระหว่างภัยพิบัติและหลังภัยพิบัติ เพื่อให้ผู้ใช้สามารถตัดสินใจและรับมือกับสถานการณ์ได้อย่างมั่นใจ
-            </p>
-            <p className="hero--section-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-              recusandae?
-            </p>
-          </div>
-        </div>
-      </section>
-
-    );
-};    
+  );
+};
 export default About
--- src/components/common/team/Team.jsx
+++ src/components/common/team/Team.jsx
@@ -5,56 +5,52 @@ const Team = () => {
   return (
     <section className="team--section">
       <div className="team--container">
-        <p className="team--title">My Team</p>
-        <h2 className="team--section--heading">My Team</h2>
+        <p className="team--title">ทีมงานของเรา</p>
+        <h2 className="team--section--heading">My Teams</h2>
       </div>
 
       <div className="team--section--container">
           <div className="team--section--card">
             <div className="team--section--img">
-              <img src="./logo512.png"/>
+              <img src="./images/image1.png"/>
             </div>
             <div className="team--section--card--content">
-              <h3 className="team--section--title">ewewewew</h3>
-              <p className="team--section--description">eewrwrewrerw</p>
+              <h3 className="team--section--title">ศุภกิจติ์ แดงชาติ</h3>
+              <p className="team--section--description">1630704862</p>
             </div>
           </div>
 
           <div className="team--section--card">
             <div className="team--section--img">
-              <img src="./logo512.png"/>
+              <img src="./images/image1.png"/>
             </div>
             <div className="team--section--card--content">
-              <h3 className="team--section--title">ewewewew</h3>
-              <p className="team--section--description">eewrwrewrerw</p>
+              <h3 className="team--section--title">ปฏิญญา จรูญชาต</h3>
+              <p className="team--section--description">1630704920</p>
             </div>
           </div>
 
           <div className="team--section--card">
             <div className="team--section--img">
-              <img src="./logo512.png"/>
+              <img src="./images/np.jpg"/>
             </div>
             <div className="team--section--card--content">
-              <h3 className="team--section--title">ewewewew</h3>
-              <p className="team--section--description">eewrwrewrerw</p>
+              <h3 className="team--section--title">ณภัทร ปิยะกุลวรศักดิ์</h3>
+              <p className="team--section--description">1630705364</p>
             </div>
           </div>
 
           <div className="team--section--card">
             <div className="team--section--img">
-              <img src="./logo512.png"/>
+              <img src="./images/pm.png"/>
             </div>
             <div className="team--section--card--content">
-              <h3 className="team--section--title">ewewewew</h3>
-              <p className="team--section--description">eewrwrewrerw</p>
+              <h3 className="team--section--title">ภูวเดช มิตรประชาชน</h3>
+              <p className="team--section--description">1630705406</p>
             </div>
           </div>
 
       </div>
-      
-
-
-
     </section>
   );
 }
