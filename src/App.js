f3833433dc3a418fab6fca6675f90ff83ce2b168
Chalotte026 <133754432+Chalotte026@users.noreply.github.com>
2023-12-08 14:00:41 +0700

    ให้ทำ หน้า paytation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Pages from './components/pages/Pages';
 import Login from './components/pages/Login'; // Import your Login component
 import Register from './components/pages/Register';
 import Project from './components/pages/Project';
+import Pay from './components/pages/Pay';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/project" element={<Project/>}></Route>
+        <Route path='/paytation' element={<Pay/>}></Route>
       </Routes>
     </Router>
   );
---
+++ src/components/pages/Pay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../common/footer/Footer";
+import SubHeader from "../common/sub_header/Sub_header";
+import Paytation from "../common/pay/Pay";
+function Pay() {
+    return (
+        <>
+            <SubHeader />
+            <Paytation/>
+            <Footer/>
+        </>
+    );
+}
+export default Pay
