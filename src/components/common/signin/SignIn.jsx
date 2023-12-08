4f3dcf720e3a01cf94889e02055a00d011069011
NapatNP <napat.piyak@bumail.net>
2023-12-08 17:40:39 +0700

    re:design หน้าsignin

--- src/components/common/signin/SignIn.jsx
+++ src/components/common/signin/SignIn.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import './sighin.css'
+
 const sighin = () => {
     return (
-        <div className="container" id="login">
+        /* <div className="container" id="login"> */
             <div className="login-content">
-                <img src="./logo512.png" alt="Your Name" className="login-image" />
-                <h1>Login</h1>
-                <form>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" />
+                <div className="login-card">
+                    <img src="./favicon.ico" alt="Your Name" className="login-image" />
+                    <h1>Login</h1>
+                    <form>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username" />
 
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" />
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" name="password" />
 
-                    <button type="submit">Login</button>
-                    <a href="/register" className="regis-btn">
-                        Register
-                    </a>
-                </form>
+                        <button type="submit">Login</button>
+                        <a href="/register" className="regis-btn">
+                            Register
+                        </a>
+                    </form>
+                </div>
             </div>
-        </div>
+        /* </div> */
     );
 };
-export default sighin
+
+export default sighin;
+
